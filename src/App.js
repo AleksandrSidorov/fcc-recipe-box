d3310079008eee760532ed88361f2f7866b7d03f
Aleksandr Sidorov <shtil80@gmail.com>
2016-10-18 16:10:24 +0700

    Main file structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const INIT_RECIPES = [
+  {recipe: "Cheese omelette", ingredients: ["eggs", "olive oil", "cheddar cheese"]},
+  {recipe: "Pancakes", ingredients: ["flour", "milk", "eggs", "butter"]}
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    if(!localStorage.getItem('_recipe_list')) {
+      this.populateStorage();
+    } else {
+      this.getStoredRecipes();
+    }
+  }
+
+  getStoredRecipes = () => {
+    this.setState({
+      data: JSON.parse(localStorage.getItem('_recipe_list'))
+    });
+  };
+
+  populateStorage = () => {
+    localStorage.setItem('_recipe_list', JSON.stringify(INIT_RECIPES));
+    this.getStoredRecipes();
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Welcome to Recipe Box</h1>
       </div>
     );
   }
